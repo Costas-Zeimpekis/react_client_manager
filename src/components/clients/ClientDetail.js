ff20386cd6f3641532d1e99638807fbf6391248d
Costas-Zeimpekis <morpheustudio@gmail.com>
2018-08-03 18:56:17 +0300

    Added Client Details

---
+++ src/components/clients/ClientDetail.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import PropTypes from 'prop-types';
+
+import Spinner from '../layout/Spinner';
+
+class ClientDetail extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default compose(
+  firestoreConnect(props => [
+    { collection: 'clients', storeAs: 'client', doc: props.match.params.id }
+  ]),
+  connect(({ firestore: { ordered } }, props) => ({
+    clients: ordered.client && ordered.client[0]
+  }))
+)(ClientDetails);
